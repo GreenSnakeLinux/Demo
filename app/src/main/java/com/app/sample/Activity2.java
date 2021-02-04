391b02e287d923b6a9f04b4b705a9de6863deca2
cyrilfou <cyrilfo@yahoo.fr>
2021-02-04 16:39:52 +0100

    Add prez

--- app/src/main/java/com/app/sample/Activity2.java
+++ app/src/main/java/com/app/sample/Activity2.java
@@ -10,11 +10,11 @@ public class Activity2 extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_2);
+        super.onCreate(savedInstanceState); // Call parent function onCreate in class Activity
+        setContentView(R.layout.activity_2); // View displayed on Activity2 creation, use the activity_2.xml file containing the graphical elements
 
-        TextView hello = findViewById(R.id.textActivity2);
-        String extra = getIntent().getStringExtra(EXTRA1);
-        hello.setText(extra);
+        TextView hello = findViewById(R.id.textActivity2); // Get the id of TextView called textActivity2 in the xml activity_2.xml
+        String extra = getIntent().getStringExtra(EXTRA1); // Get the value of the Tag EXTRA1
+        hello.setText(extra); // Set the text in the TextView
     }
 }
--- app/src/main/java/com/app/sample/MainActivity.java
+++ app/src/main/java/com/app/sample/MainActivity.java
@@ -8,35 +8,42 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    public static final String EXTRA1 = "extra1";
+    public static final String EXTRA1 = "extra1"; // Tag to pass in Intent to retrieve the value
     int m_demo = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState); // Call parent function onCreate in class Activity
+        setContentView(R.layout.activity_main); // View displayed on MainActivity creation, use the activity_main.xml file containing the graphical elements
 
         switch (m_demo) {
             case 1:
+                // Create an Intent to call the class Activity2
                 Intent myIntent = new Intent(this, Activity2.class);
-                myIntent.putExtra(EXTRA1, "Hello activity 2");
-                startActivity(myIntent);
+                // Pass in parameter the Tag/value used by the class Activity2
+                myIntent.putExtra(EXTRA1, "Hello activity 2"); // EXTRA1 is a Tag and can be anything, it will be use Activity2 to get the value.
+                startActivity(myIntent); // Start the Activity define in the Intent (Activity2)
                 break;
 
             case 2:
                 try {
+                    // Create an Intent with the action to perform
                     Intent intent = new Intent(Intent.ACTION_VIEW);
+                    // It will list all activity that handle the type 'Directory' : Meaning a file manager
                     intent.setType(DocumentsContract.Document.MIME_TYPE_DIR);
-                    startActivity(intent);
+                    startActivity(intent); // Start the Activity define in the Intent, if several, the user will choose
 
-                    finish();
+                    finish(); // Finish current activity
                 } catch (Exception e) {
+                    // Catch any exception in case there is no Activity that can handle the managing of directory
                     e.printStackTrace();
+                    // Toast: Display a message to the user
                     Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
                 }
                 break;
 
             default:
+                // Nothing, it will just display the layout activity_main.xml
                 break;
         }
     }
